4b674621133d74c09eec1440d182d5eb65665b49
samimhamza <samim.hamza7@gmail.com>
2024-04-29 20:18:36 +0430

    localStorage Error Fixed

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -7,7 +7,7 @@ import "mantine-datatable/styles.layer.css";
 import "./layout.css";
 import "@mantine/notifications/styles.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 import { MantineProvider, createTheme } from "@mantine/core";
 import { ColorSchemeScript } from "@mantine/core";
@@ -22,87 +22,89 @@ import { getServerSession } from "next-auth";
 import { Metadata } from "next";
 
 export async function generateStaticParams() {
-  return languages.map((lng) => ({ lng }));
+	return languages.map((lng) => ({ lng }));
 }
 
 const theme = createTheme({
-  colors: {
-    primary: [
-      "#f1f0f9",
-      "#deddec",
-      "#bcb9db",
-      "#9692cb",
-      "#7670bc",
-      "#635bb4",
-      "#5951b1",
-      "#49429c",
-      "#413b8c",
-      "#36327c",
-    ],
-    green: [
-      "#f3f8f2",
-      "#e5ede4",
-      "#c7d9c5",
-      "#a6c5a2",
-      "#8bb486",
-      "#78a973",
-      "#6ea568",
-      "#5d9057",
-      "#527f4c",
-      "#436f3f",
-    ],
-    yellow: [
-      "#fefde4",
-      "#faf8d1",
-      "#f4efa6",
-      "#eee776",
-      "#e8df4e",
-      "#e5db34",
-      "#e4d825",
-      "#cabf15",
-      "#b3aa0a",
-      "#9a9200",
-    ],
-  },
-  primaryColor: "primary",
-  breakpoints: {
-    xs: "30em",
-    sm: "48em",
-    md: "64em",
-    lg: "74em",
-    xl: "90em",
-  },
+	colors: {
+		primary: [
+			"#f1f0f9",
+			"#deddec",
+			"#bcb9db",
+			"#9692cb",
+			"#7670bc",
+			"#635bb4",
+			"#5951b1",
+			"#49429c",
+			"#413b8c",
+			"#36327c",
+		],
+		green: [
+			"#f3f8f2",
+			"#e5ede4",
+			"#c7d9c5",
+			"#a6c5a2",
+			"#8bb486",
+			"#78a973",
+			"#6ea568",
+			"#5d9057",
+			"#527f4c",
+			"#436f3f",
+		],
+		yellow: [
+			"#fefde4",
+			"#faf8d1",
+			"#f4efa6",
+			"#eee776",
+			"#e8df4e",
+			"#e5db34",
+			"#e4d825",
+			"#cabf15",
+			"#b3aa0a",
+			"#9a9200",
+		],
+	},
+	primaryColor: "primary",
+	breakpoints: {
+		xs: "30em",
+		sm: "48em",
+		md: "64em",
+		lg: "74em",
+		xl: "90em",
+	},
 });
 
 export const metadata: Metadata = {
-  title: "GPF",
-  description: "This is an MIS for GPF",
+	title: "GPF",
+	description: "This is an MIS for GPF",
 };
 
 export default async function RootLayout({
-  children,
-  params: { lng },
+	children,
+	params: { lng },
 }: {
-  children: React.ReactNode;
-  params: {
-    lng: string;
-  };
+	children: React.ReactNode;
+	params: {
+		lng: string;
+	};
 }) {
-  const session = await getServerSession(authOptions);
-  return (
-    <html lang={lng} dir={dir(lng)}>
-      <head>
-        <ColorSchemeScript defaultColorScheme="light" />
-      </head>
-      <body className={inter.className}>
-        <AuthProvider session={session}>
-          <MantineProvider theme={theme}>
-            <Notifications position="bottom-left" />
-            {children}
-            <Toaster />
-          </MantineProvider>
-        </AuthProvider>
-      </body>
-    </html>
-  );
+	const session = await getServerSession(authOptions);
+	return (
+		<html lang={lng} dir={dir(lng)}>
+			<head>
+				<ColorSchemeScript defaultColorScheme="light" />
+			</head>
+			<body
+			//  className={inter.className}
+			>
+				<AuthProvider session={session}>
+					<MantineProvider theme={theme}>
+						<Notifications position="bottom-left" />
+						{children}
+						<Toaster />
+					</MantineProvider>
+				</AuthProvider>
+			</body>
+		</html>
+	);
 }
--- customHooks/useAxios.tsx
+++ customHooks/useAxios.tsx
@@ -4,66 +4,64 @@ import { useCallback } from "react";
 import axios from "axios";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useCookies } from "react-cookie";
+import { readLocalStorageValue } from "@mantine/hooks";
 
 export const useAxios: any = () => {
-  const router = useRouter();
-  const [cookies, setCookie] = useCookies(["office"]);
+	const router = useRouter();
+	const office = readLocalStorageValue({ key: "office" });
+	const { data: session } = useSession();
 
-  const { data: session } = useSession();
+	const callAPI = useCallback(
+		async ({ method, url, params, data, headers }: any) => {
+			let response = null;
+			let loading = false;
+			let error = null;
+			let status = 404;
+			const api = axios.create({
+				baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+			});
+			let defaultHeaders = {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+				...headers,
+				Authorization: `Bearer ${session?.user.token}`,
+			};
 
-  const callAPI = useCallback(
-    async ({ method, url, params, data, headers }: any) => {
-      let response = null;
-      let loading = false;
-      let error = null;
-      let status = 404;
+			try {
+				loading = true;
+				const rawResponse = await api.request({
+					method,
+					url,
+					params: {
+						...params,
+						office: office ? office : session?.user.office_id,
+					},
+					headers: {
+						...defaultHeaders,
+						headers,
+					},
+					data,
+				});
 
-      const api = axios.create({
-        baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-      });
-      let defaultHeaders = {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        ...headers,
-        Authorization: `Bearer ${session?.user.token}`,
-      };
+				status = rawResponse.status;
+				response = rawResponse.data;
+				if (rawResponse.status == 401) {
+					await signOut({
+						redirect: false,
+					});
+					router.push("/auth/login");
+					return;
+				}
+			} catch (err: any) {
+				if (err.response.status) status = err.response.status;
+				error = err;
+			} finally {
+				loading = false;
+			}
 
-      try {
-        loading = true;
-        const rawResponse = await api.request({
-          method,
-          url,
-          params: {
-            ...params,
-            office: cookies?.office ? cookies.office : session?.user.office_id,
-          },
-          headers: {
-            ...defaultHeaders,
-            headers,
-          },
-          data,
-        });
-
-        status = rawResponse.status;
-        response = rawResponse.data;
-        if (rawResponse.status == 401) {
-          await signOut({
-            redirect: false,
-          });
-          router.push("/auth/login");
-          return;
-        }
-      } catch (err: any) {
-        if (err.response.status) status = err.response.status;
-        error = err;
-      } finally {
-        loading = false;
-      }
-
-      return { response, status, error, loading };
-    },
-    [cookies.office]
-  );
-  return callAPI;
+			return { response, status, error, loading };
+		},
+		[]
+	);
+	return callAPI;
 };
