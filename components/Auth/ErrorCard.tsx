1be905f86d83171602f6566044d3c73916ae0131
Samim Hamza <60222233+samimhamza@users.noreply.github.com>
2024-05-03 00:54:28 +0430

    Add files via upload

---
+++ components/Auth/ErrorCard.tsx
@@ -0,0 +1,16 @@
+import { RxExclamationTriangle } from "react-icons/rx";
+import CardWrapper from "./CardWrapper";
+
+export const ErrorCard = () => {
+  return (
+    <CardWrapper
+      headerLabel="Oops! Something went wrong!"
+      backButtonHref="/auth/login"
+      backButtonLabel="Back to login"
+    >
+      <div className="w-full flex justify-center items-center">
+        <RxExclamationTriangle className="text-destructive" />
+      </div>
+    </CardWrapper>
+  );
+};
